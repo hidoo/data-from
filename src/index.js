726493652868749b39871f3eeff1749400528b76
marunagahideo <hidoooo@gmail.com>
2018-08-28 14:55:42 +0900

    Fixed API exports.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import fromFiles from './fromFiles';
 import fromFrontMatter from './fromFrontMatter';
 import fromString from './fromString';
 
+export {fromFiles};
+export {fromFrontMatter};
+export {fromString};
+
 export default {
   fromFiles,
   fromFrontMatter,
