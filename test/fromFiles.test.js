f4fe46899680b2cc006c9fd13661940258fdc28d
hidoo <hidoooo@gmail.com>
2020-05-11 15:04:38 +0900

    test(fromfiles): fix test case

--- test/fromFiles.test.js
+++ test/fromFiles.test.js
@@ -1,20 +1,21 @@
 /* eslint max-len: 0, no-magic-numbers: 0 */
 
 import assert from 'assert';
+import path from 'path';
 import Handlebars from 'handlebars';
 import fromFiles from '../src/fromFiles';
 
 describe('fromFiles', () => {
 
   it('should return empty object if argument "pattern" matched files not readed.', () => {
-    const result = fromFiles(`${__dirname}/fixtures/not_exists.{json,json5,yaml}`);
+    const result = fromFiles(path.join(__dirname, 'fixtures/not_exists.{json,json5,yaml}'));
 
     assert.deepStrictEqual(result, {});
   });
 
   it('should throw Error if argument "pattern" matched files are invalid format.', () => {
     try {
-      fromFiles(`${__dirname}/fixtures/invalid.{json,json5,yaml}`);
+      fromFiles(path.join(__dirname, 'fixtures/invalid.{json,json5,yaml}'));
     }
     catch (error) {
       assert(error instanceof Error);
@@ -22,7 +23,7 @@ describe('fromFiles', () => {
   });
 
   it('should return object if argument "pattern" matched files are valid format.', () => {
-    const result = fromFiles(`${__dirname}/fixtures/valid.{json,json5,yaml}`);
+    const result = fromFiles(path.join(__dirname, 'fixtures/valid.{json,json5,yaml}'));
 
     assert.deepStrictEqual(result, {
       validJSON: {hoge: 'hoge'},
@@ -32,7 +33,7 @@ describe('fromFiles', () => {
   });
 
   it('should return object if argument "pattern" matched files are valid format that includes Handlebars Template.', () => {
-    const result = fromFiles(`${__dirname}/fixtures/template.{json,json5,yaml}`);
+    const result = fromFiles(path.join(__dirname, 'fixtures/template.{json,json5,yaml}'));
 
     assert.deepStrictEqual(result, {
       validJSON: {hoge: 'hoge', fuga: 'hoge'},
@@ -43,7 +44,7 @@ describe('fromFiles', () => {
 
   it('should return object if argument "pattern" matched files are valid format that includes Handlebars Template and argument "options.context" is object.', () => {
     const context = {data: {hoge: 'piyo'}},
-          result = fromFiles(`${__dirname}/fixtures/template-context.{json,json5,yaml}`, {context});
+          result = fromFiles(path.join(__dirname, 'fixtures/template-context.{json,json5,yaml}'), {context});
 
     assert.deepStrictEqual(result, {
       validJSON: {hoge: 'hoge', fuga: 'hoge', piyo: 'piyo'},
@@ -58,7 +59,7 @@ describe('fromFiles', () => {
     // add custom helper
     hbs.registerHelper('wrapBrackets', (value) => new Handlebars.SafeString(`[[ ${value} ]]`));
 
-    const result = fromFiles(`${__dirname}/fixtures/template-custom-handlebars.{json,json5,yaml}`, {handlebars: hbs});
+    const result = fromFiles(path.join(__dirname, 'fixtures/template-custom-handlebars.{json,json5,yaml}'), {handlebars: hbs});
 
     assert.deepStrictEqual(result, {
       validJSON: {hoge: 'hoge', fuga: '[[ hoge ]]'},
